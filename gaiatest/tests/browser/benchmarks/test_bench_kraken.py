7381dba225f75baebdd7bcd641af9e20576b1388
Nicolas B. Pierron <nicolas.b.pierron@mozilla.com>
2013-04-13 18:43:40 -0700

    Add kraken and sunspider benchmark tests.

---
+++ gaiatest/tests/browser/benchmarks/test_bench_kraken.py
@@ -0,0 +1,71 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+import json
+import time
+from gaiatest import GaiaTestCase
+from gaiatest.apps.browser.app import Browser
+
+
+class TestBenchKraken(GaiaTestCase):
+
+    _start_page = 'http://people.mozilla.com/~npierron/kraken/hosted/index.html'
+    _run_locator = ('css selector', '#results > p > a')
+
+    _console_locator = ('id', 'console')
+
+    def setUp(self):
+        GaiaTestCase.setUp(self)
+
+        if self.wifi:
+            self.data_layer.enable_wifi()
+            self.data_layer.connect_to_wifi(self.testvars['wifi'])
+
+    def test_kraken(self):
+        # https://github.com/mozilla/gaia-ui-tests/issues/450
+        browser = Browser(self.marionette)
+        browser.launch()
+
+        browser.go_to_url(self._start_page)
+        browser.switch_to_content()
+        self.verify_home_page()
+
+        run_link = self.marionette.find_element(*self._run_locator)
+
+        # wait 30s, to let the system settle.
+        time.sleep(30)
+
+        # start the benchmark
+        self.marionette.tap(run_link)
+
+        # Switch to the chrome, because the page will be automatically
+        # redirected as soon as the benchmark is complete
+        browser.switch_to_chrome()
+
+        # wait 8 minutes, to let the becnhmark complete.
+        time.sleep(8 * 60)
+        browser.switch_to_content()
+        self.verify_finished()
+
+        self.print_results()
+
+    def verify_home_page(self):
+        self.wait_for_element_present(*self._run_locator)
+        link = self.marionette.find_element(*self._run_locator)
+        self.assertTrue(link.text == 'Begin', 'The kraken page is not rendered.')
+
+    def verify_finished(self):
+        self.wait_for_element_present(*self._run_locator)
+        link = self.marionette.find_element(*self._run_locator)
+        self.assertTrue(link.text == 'Run Again', 'The kraken page is not rendered.')
+
+    def print_results(self):
+        result = self.marionette.execute_script("""
+          return document.getElementById('console').innerHTML
+                   .replace(/<.?a[^>]*>/g, '').split('<br>')
+        """)
+
+        print "\n\nShell-like kraken results:"
+        print "\n".join(result)
+        print "End of shell-like results."
---
+++ gaiatest/tests/browser/benchmarks/test_bench_sunspider.py
@@ -0,0 +1,70 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+import json
+import time
+from gaiatest import GaiaTestCase
+from gaiatest.apps.browser.app import Browser
+
+
+class TestBenchSunspider(GaiaTestCase):
+
+    _start_page = 'http://people.mozilla.com/~npierron/sunspider/hosted/'
+    _start_now_locator = ('css selector', 'body > p > a')
+
+    _console_locator = ('id', 'console')
+
+    def setUp(self):
+        GaiaTestCase.setUp(self)
+
+        if self.wifi:
+            self.data_layer.enable_wifi()
+            self.data_layer.connect_to_wifi(self.testvars['wifi'])
+
+    def test_sunspider(self):
+        # https://github.com/mozilla/gaia-ui-tests/issues/450
+        browser = Browser(self.marionette)
+        browser.launch()
+
+        browser.go_to_url(self._start_page)
+        browser.switch_to_content()
+        self.verify_home_page()
+        start_now_link = self.marionette.find_element(*self._start_now_locator)
+
+        # wait 30s, to let the system settle.
+        time.sleep(15)
+
+        # scroll to the link location & start the benchmark
+        start_now_link.single_tap()
+        self.marionette.tap(start_now_link)
+
+        # Switch to the chrome, because the page will be automatically
+        # redirected as soon as the benchmark is complete
+        browser.switch_to_chrome()
+
+        # wait 4 minutes, to let the becnhmark complete.
+        time.sleep(4 * 60)
+        browser.switch_to_content()
+        self.verify_finished()
+
+        self.print_results()
+
+    def verify_home_page(self):
+        self.wait_for_element_present(*self._start_now_locator)
+        link = self.marionette.find_element(*self._start_now_locator)
+        self.assertTrue(link.text == 'Start Now!', 'The sunspider page is not rendered.')
+
+    def verify_finished(self):
+        self.wait_for_element_present(*self._console_locator)
+        result = self.marionette.find_element(*self._console_locator)
+        self.assertTrue(result.is_displayed, 'Is that the result page ?!')
+
+    def print_results(self):
+        result = self.marionette.execute_script("""
+          return document.getElementById('console').innerHTML.split('<br>');
+        """)
+
+        print "\n\nShell-like sunspider results:"
+        print "\n".join(result)
+        print "End of shell-like results."
